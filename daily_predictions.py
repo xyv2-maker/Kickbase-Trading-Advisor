7c10b8027844cb311781063a9bb040e3aeb499e9
xyv2-maker <jamie.junen2@gmail.com>
2026-09-18 21:52:25 +0200

    Update daily_predictions.py

--- daily_predictions.py
+++ daily_predictions.py
@@ -55,9 +55,9 @@ pd.set_option("display.width", 1000)
 # Adjust these settings to your preferences
 
 competition_ids = [1]                   # 1 = Bundesliga, 2 = 2. Bundesliga, 3 = La Liga
-league_name = "Die 10 Nuggatschleusen"  # Name of your league, must be exact match, can be done via env or hardcoded
-start_budget = 50_000_000               # Starting budget of your league, used to calculate current budgets of other managers
-league_start_date = "2025-08-08"        # Start date of your league, used to filter activities, format: YYYY-MM-DD
+league_name = "Kickbase Season 26/27"  # Name of your league, must be exact match, can be done via env or hardcoded
+start_budget = 150_000_000               # Starting budget of your league, used to calculate current budgets of other managers
+league_start_date = "2026-08-14"        # Start date of your league, used to filter activities, format: YYYY-MM-DD
 email = os.getenv("EMAIL_USER")         # Email to send recommendations to, can be the same as EMAIL_USER or different
 
 # ---------------------------------------------------
